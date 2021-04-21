441b7fd38b1bbdcfdd959e83d91bfea2229079ee
limajima <htfsamuel25@hotmail.com>
2021-04-22 01:22:11 +0700

    add dist

---
+++ dist/precache-manifest.de11747aee878523a02282d5728959fe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "083078f9c4920ad54392",
+    "url": "/crypto-tracker-dashboard/css/app.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/crypto-tracker-dashboard/fonts/fontawesome-webfont.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/crypto-tracker-dashboard/fonts/fontawesome-webfont.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/crypto-tracker-dashboard/fonts/fontawesome-webfont.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/crypto-tracker-dashboard/fonts/fontawesome-webfont.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/crypto-tracker-dashboard/img/fontawesome-webfont.svg"
+  },
+  {
+    "revision": "6745e5b1ec32cf12bb0ea0595592b034",
+    "url": "/crypto-tracker-dashboard/index.html"
+  },
+  {
+    "revision": "083078f9c4920ad54392",
+    "url": "/crypto-tracker-dashboard/js/app.js"
+  },
+  {
+    "revision": "22a5fdeae0ecc553ef79",
+    "url": "/crypto-tracker-dashboard/js/chunk-4e7d639c.js"
+  },
+  {
+    "revision": "9196f48d89b8056d653e",
+    "url": "/crypto-tracker-dashboard/js/chunk-60bb78bb.js"
+  },
+  {
+    "revision": "b6931a3a890e474f8fa0",
+    "url": "/crypto-tracker-dashboard/js/chunk-vendors.js"
+  },
+  {
+    "revision": "59b4fc26343074884653a2af117147f9",
+    "url": "/crypto-tracker-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/crypto-tracker-dashboard/robots.txt"
+  }
+]);
